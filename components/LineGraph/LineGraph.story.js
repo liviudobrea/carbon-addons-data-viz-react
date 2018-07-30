e42b62dd1535b904ba8b32e88bc846ee9f16ed94
Liviu Dobrea <dobrealiviuc@gmail.com>
2018-07-30 13:23:17 +0300

    chore(): Update linegraph log chart and stories; Fix tooltip handling for falsy values on heading prop;

--- components/LineGraph/LineGraph.story.js
+++ components/LineGraph/LineGraph.story.js
@@ -1,47 +1,12 @@
 import React, { Component } from 'react';
+import { extent } from 'd3';
 import { storiesOf, action } from '@storybook/react';
 import LineGraph from './LineGraph';
 import flatMap from 'lodash/flatMap';
-import flatten from 'lodash/flatten';
-
-const defaultDataSets = [
-  [
-    [1491, 1506816000000],
-    [1644, 1509494400000],
-    [1019, 1512086400000],
-    [836, 1514764800000],
-    [1009, 1517443200000],
-    [1461, 1519862400000],
-  ],
-  [
-    [185, 1506816000000],
-    [120, 1509494400000],
-    [55, 1512086400000],
-    [89, 1514764800000],
-    [109, 1517443200000],
-    [61, 1519862400000],
-  ],
-  [
-    [29.962162162162162, 1506816000000],
-    [43.15833333333333, 1509494400000],
-    [38.30909090909091, 1512086400000],
-    [24.382022471910112, 1514764800000],
-    [58.12, 1517443200000],
-    [91.34, 1519862400000],
-  ],
-  [
-    [5543, 1506816000000],
-    [5179, 1509494400000],
-    [2107, 1512086400000],
-    [2170, 1514764800000],
-    [3921, 1517443200000],
-    [4389, 1519862400000],
-  ],
-];
 
 class LineGraphContainer extends Component {
   state = {
-    data: this.createData(12).sort(function(a, b) {
+    data: createData(12).sort(function(a, b) {
       return a[a.length - 1] - b[b.length - 1];
     }),
     datasets: [],
@@ -70,48 +35,13 @@ class LineGraphContainer extends Component {
     window.clearInterval(this.interval);
   }
 
-  createData(num) {
-    let data = [];
-    for (let i = 0; i < num; i++) {
-      let tempArr = [];
-      let d = new Date();
-      let randomNum = Math.floor(Math.random() * 1000 + 1);
-      let randomNumTwo = Math.floor(Math.random() * 1000 + 1);
-      let randomNumThree = Math.floor(Math.random() * 1000 + 1);
-      d = d.getTime() - i * 3000;
-      tempArr.push(randomNum, randomNumTwo, randomNumThree, d);
-      data.push(tempArr);
-    }
-
-    return data;
-  }
-
-  createDataset(num) {
-    const { datasets } = this.props;
-    return Array.from({ length: num }, (v, k) =>
-      Array.from({ length: datasets[k].length }, (v, idx) => {
-        return flatten([
-          datasets[k][idx]
-            .slice(0, datasets[k][idx].length - 1)
-            .map((v, idx) => {
-              if (idx % 2 === 0) {
-                return v + Math.floor(Math.random() * 1000 + 1);
-              }
-              return Math.abs(v - Math.floor(Math.random() * 1000 + 1));
-            }),
-          datasets[k][idx][datasets[k][idx].length - 1],
-        ]);
-      })
-    );
-  }
-
   updateData(i) {
     let data = [];
     let datasets = [];
     if (this.props.datasets.length) {
-      datasets = this.createDataset(this.props.datasets.length);
+      datasets = createDataSets(this.props.datasets.length);
     } else {
-      data = this.createData(12).sort(function(a, b) {
+      data = createData(12).sort(function(a, b) {
         return a[a.length - 1] - b[b.length - 1];
       });
     }
@@ -152,6 +82,39 @@ class LineGraphContainer extends Component {
     return <LineGraph {...props} />;
   }
 }
+
+const createData = num => {
+  let data = [];
+  for (let i = 0; i < num; i++) {
+    let tempArr = [];
+    let d = new Date();
+    let randomNum = Math.floor(Math.random() * 1000 + 1);
+    d.setFullYear(d.getFullYear() + i);
+    tempArr.push(randomNum, d.getTime());
+    data.push(tempArr);
+  }
+
+  return data;
+};
+
+const createDataSets = (lines, sets) => {
+  return Array.from({ length: lines }, () =>
+    Array.from({ length: sets }, (v, idx) => {
+      const date = new Date();
+      date.setMonth(date.getMonth() + idx);
+      if (idx % 2 === 0) {
+        return [
+          idx + Math.floor(Math.random() * 1000 * 1000 * 1000),
+          date.getTime(),
+        ];
+      }
+      return [
+        Math.abs(idx - Math.floor(Math.random() * 1000 * 1000 * 1000)),
+        date.getTime(),
+      ];
+    })
+  );
+};
 
 storiesOf('LineGraph', module)
   .addWithInfo(
@@ -226,48 +189,63 @@ storiesOf('LineGraph', module)
   )
   .addWithInfo('Static', ` Static Example. `, () => (
     <LineGraph
-      datasets={defaultDataSets}
-      showTooltip={false}
+      timeFormat="%m-%Y"
+      data={createData(4)}
       showLegend
-      seriesLabels={Array.from(
-        { length: defaultDataSets.length },
-        (v, k) => `Series ${k}`
-      )}
+      hoverOverlay
+      multiValueTooltip
+      formatValue={v => `${v / 1000}k`}
       onHover={action('Hover')}
       onMouseOut={action('Mouseout')}
       onBlur={action('Blur')}
     />
   ))
+  .addWithInfo('Custom xDomain', ` Static Example. `, () => {
+    const datasets = createDataSets(4, 5);
+    const [min, max] = extent(flatMap(datasets).map(d => d[1]));
+    const minDate = new Date(min);
+    minDate.setDate(minDate.getDate() - 5);
+    const maxDate = new Date(max);
+    maxDate.setDate(maxDate.getDate() + 5);
+    return (
+      <LineGraph
+        timeFormat="%m-%Y"
+        datasets={datasets}
+        seriesLabels={Array.from({ length: datasets }, (v, k) => `Series ${k}`)}
+        xDomain={[minDate.getTime(), maxDate.getTime()]}
+        showLegend
+        hoverOverlay
+        multiValueTooltip
+        onHover={action('Hover')}
+        onMouseOut={action('Mouseout')}
+        onBlur={action('Blur')}
+      />
+    );
+  })
   .addWithInfo('Number values for X', ` Static Example. `, () => (
     <LineGraph
-      datasets={defaultDataSets}
+      datasets={createDataSets(4, 5).map(set => set.map((v, i) => [v[0], i]))}
       onHover={action('Hover')}
       onMouseOut={action('Mouseout')}
       onBlur={action('Blur')}
-      seriesLabels={Array.from(
-        { length: defaultDataSets.length },
-        (v, k) => `Series ${k}`
-      )}
+      hoverOverlay
+      seriesLabels={Array.from({ length: 4 }, (v, k) => `Series ${k}`)}
       showLegend
       isXTime={false}
     />
   ))
   .addWithInfo('Logarithmic', ` Static Example. `, () => (
     <LineGraph
-      datasets={defaultDataSets}
+      datasets={createDataSets(4, 5)}
       scaleType="log"
       // onHover={action('Hover')}
       // onMouseOut={action('Mouseout')}
       // onBlur={action('Blur')}
-      seriesLabels={Array.from(
-        { length: defaultDataSets.length },
-        (v, k) => `Series ${k}`
-      )}
+      seriesLabels={Array.from({ length: 4 }, (v, k) => `Series ${k}`)}
       showLegend
       hoverOverlay
       multiValueTooltip
-      timeFormat="%b"
-      isXTime={true}
+      timeFormat="%m-%Y"
       formatTooltipData={({ datasets, data, seriesLabels, label, color }) => {
         const dataSets = flatMap(datasets, v =>
           v.filter(d => d.includes(data[data.length - 1]))
@@ -281,22 +259,49 @@ storiesOf('LineGraph', module)
       }}
     />
   ))
+  .addWithInfo('Logarithmic - Custom Y Domain', ` Static Example. `, () => {
+    const datasets = createDataSets(4, 5);
+    const flattened = flatMap(datasets).map(d => d[0]);
+    const { 0: min, [flattened.length - 1]: max } = flattened.sort(
+      (a, b) => a - b
+    );
+    return (
+      <LineGraph
+        datasets={datasets}
+        scaleType="log"
+        yDomain={[min / 1.005, max * 1.005]}
+        yAxisTicks={3}
+        seriesLabels={Array.from({ length: datasets }, (v, k) => `Series ${k}`)}
+        showLegend
+        hoverOverlay
+        multiValueTooltip
+        timeFormat="%m-%Y"
+        formatTooltipData={({ datasets, data, seriesLabels, label, color }) => {
+          const dataSets = flatMap(datasets, v =>
+            v.filter(d => d.includes(data[data.length - 1]))
+          );
+          return dataSets.map((dataSet, idx) => ({
+            data: dataSet[0],
+            label:
+              seriesLabels && seriesLabels.length ? seriesLabels[idx] : label,
+            color: color[idx],
+          }));
+        }}
+      />
+    );
+  })
   .addWithInfo('Logairthmic - Updating', () => (
     <LineGraphContainer
-      datasets={defaultDataSets}
+      datasets={createDataSets(4, 5)}
       scaleType="log"
       // onHover={action('Hover')}
       // onMouseOut={action('Mouseout')}
       // onBlur={action('Blur')}
-      seriesLabels={Array.from(
-        { length: defaultDataSets.length },
-        (v, k) => `Series ${k}`
-      )}
+      seriesLabels={Array.from({ length: 4 }, (v, k) => `Series ${k}`)}
       showLegend
       hoverOverlay
       multiValueTooltip
       timeFormat="%b"
-      isXTime={true}
       formatTooltipData={({ datasets, data, seriesLabels, label, color }) => {
         const dataSets = flatMap(datasets, v =>
           v.filter(d => d.includes(data[data.length - 1]))
